40a4b45d9e6402e6255a888201e957b429c7dfa2
khalid <khalidalanzi@Khalids-MacBook-Air.local>
2023-11-20 14:34:44 +0300

    v final2

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let singoutbtn = document.getElementById("singoutbtn")
 let lname = document.getElementById("lname")
 let local = localStorage.getItem("Name");
 let welmsg = document.getElementById("welmsg")
-
+let info = document.getElementById("info")
 if (local){
     singinbtn.style.cssText = "display: none"
     singupbtn.style.cssText = "display: none"
     welmsg.style.cssText = "display: none"
     singoutbtn.style.cssText = "disfplay: flex"
+    info.style.cssText = "display: grid"
+
+
     lname.innerText = local
 }
 
